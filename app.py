c5286634db86c7b1f5d9a1f619a063d0c68373f1
heanuea <g00318763@gmit.ie>
2017-10-27 19:38:45 +0100

    boom

---
+++ app.py
@@ -0,0 +1,14 @@
+#FLASK_APP=app.py flask run
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "Hello Flask"
+
+
+    if __name__=="__main__":
+        app.run()
+
+
